2842aeac86943b2cccd63c08654080d6deb5133d
Igor Knyazev <igor.s.knyazev@gmail.com>
2022-02-13 17:18:54 +0700

    Destroy old bullets

---
+++ Assets/ScriptsAndPrefabs/Player/Weapon/BulletAge_S.cs
@@ -0,0 +1,40 @@
+using Unity.Entities;
+
+namespace ScriptsAndPrefabs.Player.Weapon {
+
+	[UpdateInGroup(typeof(SimulationSystemGroup))]
+	public class BulletAge_S : SystemBase {
+
+		private BeginSimulationEntityCommandBufferSystem beginSimECB;
+
+		protected override void OnCreate() {
+
+			this.beginSimECB = World.GetOrCreateSystem<BeginSimulationEntityCommandBufferSystem>();
+
+		}
+
+		protected override void OnUpdate() {
+
+			var commandBuffer = this.beginSimECB.CreateCommandBuffer().AsParallelWriter();
+			var deltaTime = Time.DeltaTime;
+			
+			Entities
+				.ForEach((Entity e, int nativeThreadIndex, ref BulletAge_C bulletAge) => {
+
+					bulletAge.age += deltaTime;
+
+					if (bulletAge.age >= bulletAge.maxAge) {
+						
+						commandBuffer.DestroyEntity(nativeThreadIndex, e);
+						
+					}
+					
+				}).ScheduleParallel();
+			
+			this.beginSimECB.AddJobHandleForProducer(Dependency);
+
+		}
+
+	}
+
+}
